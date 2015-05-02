86ea1d19642b5201c9f424bd66ae205c9a6ef36d
Mikhail <lapshin.michail@gmail.com>
2015-05-02 17:06:17 +0300

    Справочник должности поиск и добавление

--- app/Справочники/Справочник должности.js
+++ app/Справочники/Справочник должности.js
@@ -6,66 +6,62 @@ function ManJobForm() {
     var self = this
             , model = P.loadModel(this.constructor.name)
             , form = P.loadForm(this.constructor.name, model);
-    
+
     self.show = function (aDesktop) {
         aDesktop ? form.showInternalFrame(aDesktop) : form.show();
     };
-    
-    self.showModal = function(aCallback) {
+
+    self.showModal = function (aCallback) {
         form.showModal(aCallback);
     };
-    
+
     model.requery(function () {
         // TODO : place your code here
     });
-    
-    form.btnAdd.onActionPerformed = function(event) {
-        if(form.mgJobs.data.length == 0 && form.tfManJobSearch.text!== '')
+
+    form.btnAdd.onActionPerformed = function (event) {
+        if (form.mgJobs.data.length == 0 && form.tfManJobSearch.text != '')
         {
-            model.qManJob.push({job_title: form.tfManJobSearch.text});             
+            model.qManJob.push({job_title: form.tfManJobSearch.text});
             form.mgJobs.data = model.qManJob;
-            model.save(function(){});
-        }
-        else if(form.mgJobs.data.length != 0)
+            model.save(function () {
+                form.tfManJobSearch.text = '';
+            });
+        } else
+        if (form.mgJobs.data.length != 0 && form.tfManJobSearch.text == '')
             model.qManJob.push({});
     };
-    form.btnDel.onActionPerformed = function(event) {
-        if (confirm("Удалить?"))
-        model.qManJob.remove(model.qManJob.cursorPos);
-    };
-    form.btnReq.onActionPerformed = function(event) {
-        if(model.modified && confirm("Сохранить изменения?")) 
-            model.save(function(){
+
+    form.btnReq.onActionPerformed = function (event) {
+        if (model.modified && confirm("Сохранить изменения?"))
+            model.save(function () {
                 model.requery();
             });
         else
             model.requery();
     };
-    form.btnSave.onActionPerformed = function(event) {
-        model.save(function(){
-                model.requery();
-            });
+    form.btnSave.onActionPerformed = function (event) {
+        model.save(function () {
+            model.requery();
+        });
+        form.tfManJobSearch.text = '';
     };
-    form.button.onActionPerformed = function(event) {
+    form.button.onActionPerformed = function (event) {
         if (model.modified && confirm('Сохранить изменения'))
             model.save();
         form.close(model.qManJob.cursor.man_job_id);
     };
-    
-    form.tfManJobSearch.onValueChange = function() {
-//        model.qManJob.params.job_find = form.tfManJobSearch.text ? form.tfManJobSearch.text : null;        
-//        model.qManJob.execute();  
+
+    form.tfManJobSearch.onValueChange = function () {
         if (form.tfManJobSearch.text !== '') {
-            var myRe = form.tfManJobSearch.text; 
-            var newArr = model.qManJob.filter(function(element, index, array) {
-                if(element.job_title.match(new RegExp(myRe, "i")))
-                    return true;
-            });            
-            form.mgJobs.data = newArr;   
-            
-//            if(form.mgJobs.data.length == 0)
-//                console.log("null");
-            
+            var myRe = form.tfManJobSearch.text;
+            var re = new RegExp(myRe, "i");
+            var newArr = model.qManJob.filter(function (element, index, array) {
+                if (element.job_title)
+                    if (element.job_title.match(re))
+                        return true;
+            });
+            form.mgJobs.data = newArr;
         } else {
             form.mgJobs.data = model.qManJob;
         }
