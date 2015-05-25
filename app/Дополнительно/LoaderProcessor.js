88a049d3e7538cf81d981268412a915ce8732d63
Mikhail Lapshin <lapshin.michail@gmail.com>
2015-05-25 11:54:22 +0300

    Доделал лоадер, запихнул в него картинку, выровнял по центру

--- app/Дополнительно/LoaderProcessor.js
+++ app/Дополнительно/LoaderProcessor.js
@@ -7,19 +7,14 @@ function LoaderProcessor() {
             , model = P.loadModel(this.constructor.name)
             , form = P.loadForm(this.constructor.name, model);
     
-//    self.show = function () {
-//        form.show();
-//    };
-    
-     self.loader = function(aForm, aGrid, aQuery){
-        //var lbLoad = new P.Label("Загрузка...");
-        //var lbLoad = form.lbLoad;
-        //lbLoad.icon = new P.Image() //Icon.load("icons/loader200x200.gif");
-        
+     self.loader = function(aForm, aGrid, aQuery){        
         var parent = aGrid.parent;
-        form.lbLoad.height = 100;
-        form.lbLoad.width = 100;
-        parent.add(form.lbLoad);// (form.lbLoad, new P.Anchors (0,200,200,100,200,0));
+        form.lbLoad.height = 200;
+        form.lbLoad.width = 200;
+        //parent.add(form.lbLoad);// (form.lbLoad, new P.Anchors (0,200,200,100,200,0));
+        var w = Math.round(parent.width / 2 - 100);
+        var h = Math.round(parent.height / 2 - 100);
+        parent.add(form.lbLoad, new P.Anchors (w,200,w,h,200,h));
 
         aGrid.visible = false;
         aQuery.requery(function(){
