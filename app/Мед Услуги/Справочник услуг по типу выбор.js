26f0fa8ead6298e52b649f6b5a76862fa6b85675
minya92 (home) <lapshin.michail@gmail.com>
2015-05-28 20:53:08 +0300

    убрал форму типы услуг, добавил функционал в справочник услуги

--- app/Мед Услуги/Справочник услуг по типу выбор.js
+++ app/Мед Услуги/Справочник услуг по типу выбор.js
@@ -85,4 +85,32 @@ function Uslugi4SelectView() {
     form.btnSelect.onActionPerformed = function(event) {
         form.close(model.qUslugiByType.cursor.usl_uslugi_id); //return usl_id
     };
+    form.btnAdd1.onActionPerformed = function(event) {
+        var newUslName = prompt('Название нового типа услуги');
+        if (newUslName)
+            model.qUslTypes.push({
+                type_name   :   newUslName,
+                type_parent :   
+                        model.qUslTypes.cursor.type_parent !== 0 ? model.qUslTypes.cursor.type_parent : null
+            });
+    };
+    form.btnAddParent.onActionPerformed = function(event) {
+        var newUslName = prompt('Название нового типа услуги');
+        if (newUslName)
+            model.qUslTypes.push({
+                type_name   :   newUslName,
+                type_parent :   
+                        model.qUslTypes.cursor.usl_types_id !== 0 ? model.qUslTypes.cursor.usl_types_id : null
+            });
+    };
+    form.btnDel1.onActionPerformed = function(event) {
+        if (model.qUslTypes.cursor.usl_types_id) {
+            if (confirm('Внимание! Так же будут удалены услуги с данным типом!\nУдалить данный тип услуги?')) {
+                model.qUslTypes.remove(model.qUslTypes.cursorPos);
+            };
+        } else {
+            alert('Невозможно удалить данный тип услуг!');
+        }
+    };
+
 }
