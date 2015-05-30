116f1ae3fce74041d4863c4ca0fc37b9a3945515
Alex <111@111.11>
2015-05-30 01:56:24 -0700

    Treat calculator still working on it

--- app/Обращения/TreatCalculator.js
+++ app/Обращения/TreatCalculator.js
@@ -41,19 +41,77 @@ function TreatCalculator() {
         return hazardRoutes[aHazardId];
     }
     
+    function checkIfUslIsApplicable(aPatient, anUsluga) {
+        var applicable = true;
+        if (anUsluga.sex && anUsluga.sex !== aPatient.sex) applicable = false;
+        if (anUsluga.limitation_age) {
+            var patientAgeYears = (new Date).getFullYear() - aPatient.date_oft_birth.getFullYear();
+            switch (anUsluga.limitation_age_type) {
+                case 1: {
+                        break
+                }
+                case 2: {//Older
+                        if (anUsluga.limitation_age <= patientAgeYears)
+                            applicable = false;
+                        break;
+                }
+                case 3: {//Younger
+                        if (anUsluga.limitation_age > patientAgeYears)
+                            applicable = false;
+                        break;
+                }
+            }
+        }
+        return applicable;
+    }
+    
     self.calculateRoute4Group = function(aPatients, anUslugi) {
-        
+        var patients = [];
+        var uslugi = {};
+        aPatients.forEach(function(aPatient) {
+            var patient = patientM.getPatientSync(aPatient);
+            patient.route = self.calculateRoute4Person(patient, anUslugi);
+            patients.push(patient);
+            patient.route.forEach(function(routeUsl) {
+                //TODO HERE
+            });
+        });
     };
     
     self.calculateRoute4Person = function(aPatient, anUslugi) {
-        var patient = patientM.getPatientSync(aPatient);
+        var patient = typeof aPatient === 'object' ? aPatient : patientM.getPatientSync(aPatient);
         patient.route = {};
         anUslugi.forEach(function(uslId) {
             var uslRoute = getUslRoute(uslId);
             patient.doHazards = uslRoute.useHazards;
             uslRoute.route.forEach(function(routeUsl) {
-                //TODO Here
+                if (checkIfUslIsApplicable(patient, routeUsl)) {
+                    patient.route[routeUsl.usl_uslugi_id] = {
+                        usl_id: routeUsl.usl_uslugi_id,
+                        usl_content: true,
+                        hazard: false
+                    };
+                }
             });
         });
+        if (patient.doHazards) {
+            patient.hazards.forEach(function(hazard) {
+                var hazRoute = getHazardRoute(hazard);
+                hazRoute.forEach(function(routeUsl) {
+                    if (checkIfUslIsApplicable(patient, routeUsl)) {
+                        if (!patient.route[routeUsl.usl_uslugi_id])
+                            patient.route[routeUsl.usl_uslugi_id] = {
+                                usl_id: routeUsl.usl_uslugi_id,
+                                usl_content: false,
+                                hazard: true
+                            };
+                        else
+                            patient.route[routeUsl.usl_uslugi_id].hazard = true;
+                    }
+                });
+            });
+        }
+        
+        return patient.route;
     };
 }
