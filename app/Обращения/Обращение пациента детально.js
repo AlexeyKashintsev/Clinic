f3f946b32936d9d3d60d500efe49d7dc0f880844
Mikhail Lapshin (work) <lapshin.michail@gmail.com>
2015-06-01 11:50:46 +0300

    Детальное обращение пациента + мелкие фиксы

---
+++ app/Обращения/Обращение пациента детально.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ * @author Mikhail
+ */
+function TreatmentDetailForm() {
+    var self = this
+            , model = P.loadModel(this.constructor.name)
+            , form = P.loadForm(this.constructor.name, model);
+    
+    self.show = function () {
+        form.show();
+    };
+    
+    self.showOnPanel = function (aPanel) {
+        aPanel.add(form.view);
+    };
+    
+    self.setPacient = function (aPacientId){
+        model.qTreatByPatient.params.patient_id = aPacientId;
+        model.qTreatByPatient.requery();
+    };
+    
+    model.requery(function () {
+        // TODO : place your code here
+    });
+    
+}
