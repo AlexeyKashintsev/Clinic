cdc35a6dd53aa8edd5098355ed7630ebb2495988
Alexey Kashintsev <ak@altsoft.biz>
2015-06-01 12:12:34 +0400

    Применение назначений

--- app/Обращения/TreatCreator.js
+++ app/Обращения/TreatCreator.js
@@ -6,6 +6,18 @@
  */ 
 function TreatCreator() {
     var self = this, model = P.loadModel(this.constructor.name);
+    var treatCalculator = new TreatCalculator();
+    
+    var routeData;
+    self.calculateRoute4Group = function(aPatients, anUslugi) {
+        routeData = treatCalculator.calculateRoute4Group(aPatients, anUslugi);
+        return routeData;
+    };
+    
+    self.calculateRoute4Person = function(aPatient, anUslugi) {
+        routeData = treatCalculator.calculateRoute4Person(aPatient, anUslugi);
+        return routeData;
+    };
     
     self.createGroupTreatment = function(aContractId) {
         model.revert();
@@ -18,21 +30,72 @@ function TreatCreator() {
         model.qTreatById.push({
             treatment_group_id: model.qTreatGroup.cursor.obr_group_id,
             treat_date: new Date(),
-            treat_status: 0
+            treat_status: 0,
+            group_data: true
         });
 //        model.save();
         return model.qTreatById.cursor.obr_treatment_id;
     };
     
-    self.createSingleTreatment = function(aPatientId) {
-        model.revert();
+    self.createSingleTreatment = function(aPatientId, aTreatGroup) {
+        if (!aTreatGroup) model.revert();
         model.qTreatById.params.treat_id = null;
         model.qTreatById.push({
             patient: aPatientId,
             treat_date: new Date(),
-            treat_status: 0
+            treat_status: 0,
+            treatment_group_id: aTreatGroup
         });
 //        model.save();
         return model.qTreatById.cursor.obr_treatment_id;
+    };
+    
+    function addUslugi(anUslugi, aTreatment) {
+        P.Logger.info('Добавление услуг');
+        anUslugi.forEach(function(usluga) {
+            model.qUslInTreat.push({
+                treat_id: aTreatment,
+                usluga_id: usluga
+//                contract_id: aContract
+            });
+        });
+    }
+    
+    function addRoute(aRoute, aTreatment) {
+        P.Logger.info('Добавление маршрута');
+        for (var j in aRoute) {
+            model.qTreatRoute.push({
+                treat_id: aTreatment,
+                usluga_id: j
+//                TODO: Здесь добавить какуие услуги и вредности
+            });
+        }
+    }
+    
+    self.applyTreatment = function(aTreat, anUslugi) {
+        if (aTreat === model.qTreatById.cursor.obr_treatment_id) {
+            P.Logger.info('Добавляем назначение');
+            if (model.qTreatById.cursor.group_data) {
+                P.Logger.info('Групповое назначение');
+                addUslugi(anUslugi, aTreat);//Добавление услуг к групповому маршруту
+                routeData.patients.forEach(function(patient) {
+                    P.Logger.info('Добавляем назначения по пациенту ' + patient.surname);
+                    var cTreat = self.createSingleTreatment(patient, model.qTreatGroup.cursor.obr_group_id);
+                    addUslugi(anUslugi, cTreat);
+                    addRoute(patient.route, cTreat);
+                });
+            } else {
+//                Добавление единичного маршрута (только один человек)
+                P.Logger.info('Одиночное назначение');
+                addUslugi(anUslugi, aTreat);
+                addRoute(routeData.patients[0].route, aTreat);
+            }
+            P.Logger.info('Завершено, сохранение');
+            model.save();
+            return true;
+        } else {
+            model.revert();
+            return false;
+        }
     };
 }
