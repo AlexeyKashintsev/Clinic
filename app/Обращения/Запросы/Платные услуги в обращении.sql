405544253cefc25aa476b84cba4b8dd11f43586a
Mikhail Lapshin (work) <lapshin.michail@gmail.com>
2015-06-01 16:10:17 +0300

    добавил в услуги и назначения дополнительные столбцы

--- app/Обращения/Запросы/Платные услуги в обращении.sql
+++ app/Обращения/Запросы/Платные услуги в обращении.sql
@@ -5,6 +5,8 @@
  * @public
  * @writable obr_uslugi
  */ 
-Select * 
+Select t1.obr_uslugi_id, t1.treat_id, t1.usluga_id, t1.contract_id, t.contr_name, t2.cost
 From obr_uslugi t1
+ Left Join buh_contracts t on t1.contract_id = t.buh_contracts_id
+ Left Join usl_cost t2 on t1.contract_id = t2.contract_id
  Where :treatId = t1.treat_id
