425779a6d2cfc28947b0ab9f2a2756c821056b0a
Alexey Kashintsev <ak@altsoft.biz>
2015-06-08 16:54:40 +0400

    Добавил запрос прайс листы

---
+++ app/Бухгалтерия/Запросы/Прайс листы.sql
@@ -0,0 +1,11 @@
+/**
+ * @author Алексей
+ * @name qPriceLists
+ * @manual
+ * @public
+ */ 
+Select * 
+From buh_contracts t1
+ Where (:comp_id = t1.company_id or :comp_id is null)
+ and (:c_act = t1.с_active or (:c_act is null and t1.с_active = true))
+ and t1.price = true
