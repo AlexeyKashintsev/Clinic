2a38317966f4c13d4f7cec60b940b3e1490b7268
Alexey Kashintsev <ak@altsoft.biz>
2015-06-10 16:11:44 +0400

    litl

--- app/Обращения/Назначения.js
+++ app/Обращения/Назначения.js
@@ -273,4 +273,9 @@ function AppointmentForm() {
         });
     }
     testData();
+    form.btnDoublePrice.onActionPerformed = function(event) {
+        if (form.mgCosts.selected[0])
+            form.mgCosts.data.push(form.mgCosts.selected[0]);
+//        form.mgCosts.redraw();
+    };
 }
