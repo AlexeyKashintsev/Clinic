c25fc6726d97878a0c384ef42c7e93a401d9ac36
Alexey Kashintsev <akashintsev@yandex.ru>
2015-06-14 20:22:22 +0300

    DataLoader begin

---
+++ app/Импорт/DataLoader.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ * @author Alexey
+ */
+function DataLoader() {
+    var self = this
+            , model = P.loadModel(this.constructor.name)
+            , form = P.loadForm(this.constructor.name, model);
+    
+    self.show = function () {
+        form.show();
+    };
+    
+    self.showModal - function (aCallback) {
+        form.showModal(aCallback);
+    };
+    
+    form.btnChooseFile.onActionPerformed = function(evt) {
+        alert('clicked!');
+    };
+}
